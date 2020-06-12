538aa6a636b0a3346113643630f70a2958e748f7
JalalMirzayev <47903248+JalalMirzayev@users.noreply.github.com>
2020-06-12 23:06:27 +0200

    Create TextField_rounded_with_icon.dart

---
+++ widgets/TextField_rounded_with_icon.dart
@@ -0,0 +1,9 @@
+```dart
+TextField(
+  style: TextStyle(fontSize: 16),
+  decoration: InputDecoration(
+    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30.0))),
+    suffixIcon: Icon(Icons.search),
+    hintText: 'Search'),
+  )
+```
